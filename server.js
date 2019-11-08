8a82e955cd8b11651af6bd5a3ffaf13446d40ebe
Lucy <lucyneatdesign@yahoo.ca>
2019-11-08 21:46:55 +0000

    Heroku

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const config = require('config');
+const config = require('config/default');
 const path = require('path');
 
 const app = express();
